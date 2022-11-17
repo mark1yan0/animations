6ca022688d7b7bf92d68193aab8f99edfcab66d1
@markiyan.kmit <markiyan.kmit@double-you.it>
2022-11-17 16:32:50 +0100

    adds more polished animation to nav and nav menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import MenuIcon from './MenuIcon';
 
 export default function Header() {
   return (
-    <header className='w-full h-12 flex items-center justify-end px-2 bg-cyan-900 mb-6 text-white'>
+    <header className='w-full h-12 flex items-center justify-end px-2 md:px-6 bg-cyan-900 mb-6 text-white'>
       <MenuIcon />
       <OffCanvasMenu />
     </header>
--- src/components/Header/variants.ts
+++ src/components/Header/variants.ts
@@ -4,36 +4,38 @@ export const offVariants: Variants = {
   opened: {
     width: '100%',
     height: '100vh',
-    transition: { type: 'tween' },
+    borderBottomLeftRadius: 0,
+    transition: { 
+      type: 'tween'
+    },
   },
   closed: {
+    borderBottomLeftRadius: '100%',
     width: 0,
     height: 0,
-    transition: { type: 'tween', when: 'afterChildren' },
+    transition: {
+      type: 'tween', when: 'afterChildren'
+    },
   },
 };
 
 export const navVariants: Variants = {
   opened: {
-    transition: { staggerChildren: 0.1, delayChildren: 0.07 },
+    transition: { staggerChildren: 0.25, delayChildren: 0.01 },
   },
   closed: {
-    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+    transition: { staggerDirection: -1 }
   },
 };
 
 export const itemVariants: Variants = {
   opened: {
     opacity: 1,
-    y: 0,
-    display: 'block',
+    y: 0
   },
   closed: {
     opacity: 0,
-    y: -10,
-    transitionEnd: {
-      display: 'none',
-    },
+    y: -10
   },
 };
 
